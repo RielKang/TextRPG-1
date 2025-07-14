84ed573a22ffc8be2efb2ec448d0707278922329
zjaxjrhkd <zjaxjrhkd100@gmail.com>
2025-07-14 14:20:18 +0900

    [feat] Scene 클래스 추가
    ### 구현할 내용
    - Scene 클래스 추가
    
    <hr>
    
    ### 구현 방법
    - DefaultScene을 추상클래스로 추가한다.
    - Scene클래스는 DefaultScene를 상속받아서 작성한다.

--- TextRPG/TextRPG/TextRPG/obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
+++ TextRPG/TextRPG/TextRPG/obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6dde01d37f94ac2a545c6f87e8c44b1b6a455c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a97851f83bf53f7d2a32904ef75797f8899b6de")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
