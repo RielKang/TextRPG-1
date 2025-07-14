fc127260f740759bd8f19ece4e167fb52cd9e977
KangIngu <rielkang99@gmail.com>
2025-07-15 08:58:42 +0900

    0858

--- TextRPG/TextRPG/TextRPG/obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
+++ TextRPG/TextRPG/TextRPG/obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a02259e7e7adb0b5298423dda45c75774bb11a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9d74ad49c0a2dc39c0f9b9523f535c8840c5cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
